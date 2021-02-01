ed1e831f6eda505a03fc5efd7f58be4633c50333
jmolina518 <jmolina518@gmail.com>
2021-01-31 20:08:34 -0600

    add getVideo function

--- scripts.js
+++ scripts.js
@@ -3,3 +3,15 @@ const canvas = document.querySelector('.photo');
 const ctx = canvas.getContext('2d');
 const strip = document.querySelector('.strip');
 const snap = document.querySelector('.snap');
+
+function getVideo() {
+  navigator.mediaDevices
+    .getUserMedia({ vide: true, audio: false })
+    .then((localMediaStream) => {
+      console.log(localMeidaStream);
+      video.src = window.URL.createURL(localMediaStream);
+      video.play();
+    });
+}
+
+getVideo();
